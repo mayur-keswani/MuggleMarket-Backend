1907b48c15fbcbde2874fc3a847e3c5d1dee401f
Mayurkeswani93 <mayurkeswani2001@gmail.com>
2021-07-10 12:48:39 +0530

    deleting photos

--- utils/deleteFileHelper.js
+++ utils/deleteFileHelper.js
@@ -1,6 +1,11 @@
 const fs = require('fs')
 const deleteFile = (filePath)=>{
-	fs.unlink(filePath)
+	fs.unlink(filePath,err=>{
+		if(err)
+			console.log("Unable to delete file...")
+		else
+			console.log("Post successful deleted from internal directory")
+    })
 }
 
 module.exports=deleteFile
