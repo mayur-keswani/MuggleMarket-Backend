4b7024e9ec3717507fefbab3e046ede282013a5f
mayur-keswani <mayurkeswani2001@gmail.com>
2023-05-28 13:05:50 +0530

    passed clusterId from env

--- utils/database.js
+++ utils/database.js
@@ -1,4 +1,4 @@
 require("dotenv").config("../.env");
 exports.mongoDB = {
-  url: `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.tcppjff.mongodb.net/${process.env.MONGODB_COLLECTION}?retryWrites=true&w=majority`,
+  url: `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.${process.env.MONGODB_CLUSTER_ID}.mongodb.net/${process.env.MONGODB_COLLECTION}?retryWrites=true&w=majority`,
 };
